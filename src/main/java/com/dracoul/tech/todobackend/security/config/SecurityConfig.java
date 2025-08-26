c0b1f708de5c978ce6784c3b83a1b8d4671df152
CoulibalyD <dramane.coulibalyd636@gmail.com>
2025-08-26 12:53:46 +0000

    new version

--- src/main/java/com/dracoul/tech/todobackend/security/config/SecurityConfig.java
+++ src/main/java/com/dracoul/tech/todobackend/security/config/SecurityConfig.java
@@ -70,8 +70,8 @@ public class SecurityConfig {
     @Bean
     public CorsFilter corsFilter() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("https://todo-1po7.onrender.com"));
-       // config.setAllowedOrigins(List.of("http://localhost:4200"));
+        config.setAllowedOrigins(List.of("https://taskpo7.onrender.com"));
+        //config.setAllowedOrigins(List.of("http://localhost:4200"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("Authorization", "Cache-Control", "Content-Type"));
         config.setExposedHeaders(List.of("Authorization"));
